3e31baa08e7d09b633b608a982129a0a27393b48
Muhammad Yasir Malkani <muhammadyasirmalkani1@gmail.com>
2025-10-13 16:07:06 +0500

    Update Navigation.tsx

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -28,7 +28,7 @@ const Navigation = () => {
         <div className="flex items-center justify-between h-16">
           {/* Logo */}
           <Link to="/" className="text-2xl font-bold gradient-text">
-            Portfolio
+            DevFort Studio
           </Link>
 
           {/* Desktop Navigation */}
